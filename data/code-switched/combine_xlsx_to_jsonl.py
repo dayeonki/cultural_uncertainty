21d850789fcdde1338325cb01dbbe0aa25ffa764
Yu (Hope) Hou <houyu@umd.edu>
2024-11-30 18:05:51 -0500

    backup local changes (do not upload)

--- data/code-switched/combine_xlsx_to_jsonl.py
+++ data/code-switched/combine_xlsx_to_jsonl.py
@@ -2,10 +2,12 @@ import pandas as pd
 import json
 
 
-xlsx_file_path = 'ko.xlsx'
-df = pd.read_excel(xlsx_file_path, header=None, names=['questions'])
+# xlsx_file_path = 'ko.xlsx'
+# df = pd.read_excel(xlsx_file_path, header=None, names=['questions'])
+xlsx_file_path = 'zh.csv'
+df = pd.read_csv(xlsx_file_path, header=None, names=['questions'])
 
-jsonl_file_path = '../subset/specific_ko.jsonl'
+jsonl_file_path = '../subset/specific_zh.jsonl'
 jsonl_data = []
 
 with open(jsonl_file_path, 'r', encoding='utf-8') as file:
@@ -13,9 +15,10 @@ with open(jsonl_file_path, 'r', encoding='utf-8') as file:
         jsonl_data.append(json.loads(line))
 
 for i in range(len(jsonl_data)):
-    jsonl_data[i]["question_codeswitched"] = df['questions'][i]
+    jsonl_data[i]['ori_question_english'] = jsonl_data[i]['question_english']
+    jsonl_data[i]["question_english"] = df['questions'][i]
 
-output_jsonl_file_path = 'specific_ko.jsonl'
+output_jsonl_file_path = 'specific_zh.jsonl'
 with open(output_jsonl_file_path, 'w', encoding='utf-8') as file:
     for entry in jsonl_data:
         file.write(json.dumps(entry, ensure_ascii=False) + '\n')
